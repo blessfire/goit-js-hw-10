010a54dbd0e0300a5b1bd5b62292d00f675decc9
Volodymyr Ionaytis <blessfire91@gmail.com>
2023-08-06 15:13:54 +0200

    check and send

--- src/index.js
+++ src/index.js
@@ -3,16 +3,63 @@ import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 
+Notiflix.Notify.init({
+  timeout: 3333,
+  position: 'center-center',
+  width: '333px',
+  fontSize: '16px',
+  cssAnimationStyle: 'from-left',
+});
+
+const selectEl = document.querySelector('.breed-select');
+const catInfoEl = document.querySelector('.cat-info');
+const loaderEl = document.querySelector('.loader');
 
 const errorMsg = 'Oops! Something went wrong! Try reloading the page!';
 
+selectEl.addEventListener('change', catInfoShow);
+
+fetchBreeds().then(breeds => {
+  renderSelect(breeds);
+  selectEl.classList.remove('is-hidden');
+  loaderEl.classList.add('is-hidden');
+}).catch(() => {
+  loaderEl.classList.add('is-hidden');
+  Notiflix.Notify.failure(errorMsg);
+});
+
+function renderSelect(breeds) {
+  const markup = breeds.map(({ name, id }) => {
+    return `<option value ='${id}'>${name}</option>`;
+  }).join();
+
+  selectEl.innerHTML = markup;
+
+  new SlimSelect({
+    select: selectEl,
+  })
+}
+
+function catInfoShow(evt) {
+  catInfoEl.computedStyleMap.opacity = 0.2;
+  loaderEl.classList.remove('is-hidden');
+  const breedId = evt.currentTarget.value;
+  fetchCatByBreed(breedId).then(breeds => {
+    catInfoEl.style.opacity = 1;
+    markupCardCats(breeds);
+    loaderEl.classList.add('is-hidden');
+  }).catch(() => {
+    loaderEl.classList.add('is-hidden');
+    Notiflix.Notify.failure(errorMsg);
+})
+}
 
 function markupCardCats(breeds) {
-    const markup = breeds.map(({ url, breeds: [{ name, temperament, description, wikipedia_url }], }) => {
-        return `img class='cat-info__cat-img' src='${url} alt='cat ${name}' width='460px'><div class="cat-info__text-box">
+    const markup = breeds.map(({ url, breeds: [{ name, temperament, description}], }) => {
+        return `<img class='cat-info__img' src='${url}' alt='cat ${name}' width='460px'><div class="cat-info__box">
         <h2 class="cat-info__tittle">${name}</h2>
         <p class="cat-info__description">${description}</p>
-        <p class="cat-info__temperament"><span class="temperament__header">Temperament:</span> ${temperament}</p>
+        <p class="cat-info__temperament"><span class="cat__subtitle">Temperament:</span> ${temperament}</p>
       </div>`}).join();
     catInfoEl.innerHTML = markup;
 }
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,10 +1,10 @@
-const API_URL = 'https://api.thecatapi.com/v1';
+const BASE_URL = 'https://api.thecatapi.com/v1';
 
 import axios from "axios";
 axios.defaults.headers.common['x-api-key'] = 'live_jtm8gwip36xcN7yYdoSeJ51IdEDqcFvAiqeounFUVaQyjCP3PutEzNpb5t5PkIrd';
 
 export function fetchBreeds() {
-    return axios.get(`${API_URL}/breeds`).then(response => {
+    return axios.get(`${BASE_URL}/breeds`).then(response => {
         if (response.status !== 200) {
             throw new Error(errorMsg);
         }
@@ -13,10 +13,10 @@ export function fetchBreeds() {
 }
 
 export function fetchCatByBreed(breedId) {
-    return axios.get(`${API_URL}/images/search?breed_ids=${breedId}`).then(response => {
+    return axios.get(`${BASE_URL}/images/search?breed_ids=${breedId}`).then(response => {
         if (response.status !== 200) {
             throw new Error(errorMsg);
         }
         return response.data;
 })
-}
+} 
